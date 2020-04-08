daf6bf918efd708ad9edab4481b77a652cb89e31
Matthew Tamayo-Rios <matthew@openlattice.com>
2020-04-07 18:58:33 -0700

    Looks like we need better kotlin formatting for intellij.

--- src/test/kotlin/com/openlattice/testing/util/S3TestingUtils.kt
+++ src/test/kotlin/com/openlattice/testing/util/S3TestingUtils.kt
@@ -10,11 +10,11 @@ import io.findify.s3mock.S3Mock
 import java.net.URL
 import java.nio.file.Paths
 
-class S3TestingUtils () {
+class S3TestingUtils {
 
     companion object {
         @JvmStatic
-        fun newTestS3Client( port: Int, region: String ) : AmazonS3 {
+        fun newTestS3Client(port: Int, region: String): AmazonS3 {
             val api = S3Mock.Builder().withPort(port).withInMemoryBackend().build()
             api.start()
             val endpoint = AwsClientBuilder.EndpointConfiguration("http://localhost:$port", region)
@@ -28,10 +28,10 @@ class S3TestingUtils () {
         }
 
         @JvmStatic
-        fun loadS3WithResourceAsFile( bucket: String, key: String, resourcePath: String, client: AmazonS3) {
-            val testResource: URL = Resources.getResource( resourcePath )
+        fun loadS3WithResourceAsFile(bucket: String, key: String, resourcePath: String, client: AmazonS3) {
+            val testResource: URL = Resources.getResource(resourcePath)
             val testResAsFile = Paths.get(testResource.toURI()).toFile()
-            client.putObject( bucket, key, testResAsFile )
+            client.putObject(bucket, key, testResAsFile)
         }
     }
 
